725fd8a7231047f4a0192d42808f925f03a4be3c
hutchinsonvd <vandykehutch@yahoo.com>
2024-06-23 14:27:00 -0400

    yea

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react'
+import { getNationalities, getNextImage, getRandomPerson } from './game';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  const [person, setPerson] = useState(null);
+  const [nationalities, setNationalities] = useState(null);
+  const [selection, setSelection] = useState(null);
+
+  useEffect(() => {
+    getNationalities().then(data => setNationalities(data));
+    updatePerson();
+  }, []);
+
+  function evaluateChoice() {
+    if (selection == person.nationality) {
+      alert('correct!');
+      updatePerson();
+    }
+    else {
+      console.log(person.nationality);
+      alert('incorrect!');
+    }
+  };
+
+  async function updatePerson() {
+    return getRandomPerson().then(data => setPerson(data));
+  }
+
+  function handleSelectChange(e) {
+    setSelection(e.target.value);
+  }
+
+  if (person != null && nationalities != null) {
+
+    return (
+      <div className="App">
+              
+                    <br></br>
+                    <img style={{ width: "22%", height: "22%" }} alt="hog" src={person.image_url} id="image"/>
+                    
+                    <br></br>
+                    <form> 
+                        <label htmlFor="nationalities">Choose a nationality: </label>
+                    
+                        <select name="nationalities" id="nationalitiesDropDown" value={selection} onChange={(e) => handleSelectChange(e)}>
+
+                          <option value="⬇️ Select a nationality ⬇️">-- Select a nationality --</option>
+                            {nationalities.map((nationality, index) => (
+                              <option key={index} value={nationality}>
+                                {nationality}
+                              </option>
+                            ))}
+                        </select>
+                    
+                        <br></br>
+                    
+                        <button type="button" onClick={() => evaluateChoice()} onKeyDown={() => evaluateChoice()}>Submit</button>    <button type="button" onClick={() => updatePerson()}>Next choice </button>
+                    </form>
+      
+      </div>
+    );
+  }
+
+return (
+  <div className="App"> Loading .. </div>
+);
+
 }
 
 export default App;
---
+++ src/game.js
@@ -0,0 +1,35 @@
+//so simple html webpage
+//it auto loads a picture from dataset
+//user has a scroll bar with all the possible nationalities to choose
+import axios from 'axios';
+
+//make a server and just deploy it to render to continue testing
+var TEST_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Bundesarchiv_Bild_183-1985-0810-023,_Christine_Wachtel,_Hildegard_Körner-Ullrich.jpg/200px-Bundesarchiv_Bild_183-1985-0810-023,_Christine_Wachtel,_Hildegard_Körner-Ullrich.jpg";
+const backendURL = process.env.BACKEND || 'http://localhost:8080';
+
+export function getNextImage() {
+
+    getRandomPerson()
+    .then(person => {
+        console.log(person);
+        var imageUrl = person.image_url;
+        document.getElementById("image").src = imageUrl;
+    });
+}
+
+export async function getRandomPerson() {
+
+    return await axios.get(backendURL + "/random")
+    .then(response => {
+        console.log(response);
+        return response.data;})
+}
+
+
+export async function getNationalities() {
+
+    return await axios.get(backendURL + "/nationalities")
+    .then(response => {
+        console.log(response);
+        return response.data;})
+}
