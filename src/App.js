90f28093d56fe7c16b2dcd9eb991eea49eb3b8b4
hutchinsonvd <vandykehutch@yahoo.com>
2024-07-07 20:50:58 -0400

    hey

--- src/App.js
+++ src/App.js
@@ -1,36 +1,157 @@
-import logo from './logo.svg';
 import './App.css';
 import React, {useEffect, useState} from 'react'
 import { getNationalities, getNextImage, getRandomPerson } from './game';
 
 function App() {
   
+  const EASY = "easy";
+  const MEDIUM = "medium";
+  const HARD = "hard";
+
+  const HELTER_SKELTER = "helterSkelter";
+  const SANDBOX = "Sandbox";
+  
+
   const [person, setPerson] = useState(null);
   const [nationalities, setNationalities] = useState(null);
   const [selection, setSelection] = useState(null);
+  const [difficulty, setDifficulty] = useState(EASY);
+  const [score, setScore] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(60);
+  const [gameMode, setGameMode] = useState(SANDBOX);
 
   useEffect(() => {
-    getNationalities().then(data => setNationalities(data));
-    updatePerson();
-  }, []);
+    if (score != 0) {
+      updatePersonAndNationalityList(difficulty);
 
-  function evaluateChoice() {
-    if (selection == person.nationality) {
+      if (gameMode === HELTER_SKELTER) {
+        setTimeLeft(t => t + 15);
+      };
+    }
+  }, [score]);
+
+
+  useEffect(() => {
+
+    if (selection == null) {
+      return;
+    }
+    if (selection === person.nationality) {
+
+      
       alert('correct!');
-      updatePerson();
+      setScore(score + 1);
+      // updatePersonAndNationalityList(difficulty);
+
+      // if (gameMode === HELTER_SKELTER) {
+      //   setTimeLeft(t => t +15);
+      // };
     }
     else {
       console.log(person.nationality);
+      
       alert('incorrect!');
+
+      if (gameMode === HELTER_SKELTER) {
+        setTimeLeft(t => t - 20);
+      }
+      else {
+        setScore(0);
+      }
+    }
+  }, [selection]);
+
+  useEffect(() => {
+    const timer = setInterval(() => setTimeLeft(t => t - 1), 1000);
+
+    getRandomPerson().then(data => {
+      setPerson(data);
+      return data;})
+    .then(data => getNationalities(EASY, data))
+    .then(data => setNationalities(data));
+
+    return () => clearInterval(timer);
+  }, []);
+
+  if (timeLeft <= 0 && gameMode === HELTER_SKELTER) {
+    alert("Time's up!");
+    setScore(0);
+    setTimeLeft(60);
+  }
+
+
+  async function evaluateChoice() {
+
+    if (selection === person.nationality) {
+
+      
+      alert('correct!');
+      setScore(score + 1);
+      // updatePersonAndNationalityList(difficulty);
+
+      // if (gameMode === HELTER_SKELTER) {
+      //   setTimeLeft(t => t +15);
+      // };
+    }
+    else {
+      console.log(person.nationality);
+      
+      alert('incorrect!');
+
+      if (gameMode === HELTER_SKELTER) {
+        setTimeLeft(t => t - 20);
+      }
+      else {
+        setScore(0);
+      }
     }
   };
 
-  async function updatePerson() {
-    return getRandomPerson().then(data => setPerson(data));
+  async function updateGameMode(gameMode, difficulty) {
+
+    return updatePersonAndNationalityList(difficulty)
+      .then(data => setGameMode(gameMode))
+      .then(data => {
+        if (gameMode === HELTER_SKELTER) {
+          
+          setTimeLeft(60);
+        }
+
+        setScore(0);
+      });
+  }
+
+  async function nextChoice(difficulty) {
+    if (gameMode === HELTER_SKELTER) {
+          
+      setTimeLeft(60);
+    }
+
+    setScore(0);
+
+    return updatePersonAndNationalityList(difficulty);
+  }
+
+  async function updatePersonAndNationalityList(difficulty) {
+    return getRandomPerson().then(data => {
+
+    setPerson(data);
+    setDifficulty(difficulty);
+
+  
+    return getNationalities(difficulty, data, score)
+        .then(data => setNationalities(data));
+    });
   }
 
   function handleSelectChange(e) {
+    console.log(e);
     setSelection(e.target.value);
+  }
+
+  async function handleButtonSelect(e) {
+    e.preventDefault();
+    setSelection(e.target.innerText.replaceAll(' ', '_'));
   }
 
   if (person != null && nationalities != null) {
@@ -42,24 +163,34 @@ function App() {
                     <img style={{ width: "22%", height: "22%" }} alt="hog" src={person.image_url} id="image"/>
                     
                     <br></br>
-                    <form> 
-                        <label htmlFor="nationalities">Choose a nationality: </label>
-                    
-                        <select name="nationalities" id="nationalitiesDropDown" value={selection} onChange={(e) => handleSelectChange(e)}>
+                    <label htmlFor="nationalities">Choose a nationality: </label>
 
-                          <option value="⬇️ Select a nationality ⬇️">-- Select a nationality --</option>
-                            {nationalities.map((nationality, index) => (
-                              <option key={index} value={nationality}>
-                                {nationality}
-                              </option>
-                            ))}
-                        </select>
+                    <br></br>
+                    <br></br>
+              
+
+                    {nationalities.map(nat => (
+                      <button style={{ marginLeft: '.25rem' }} key={nat} value={nat} onClick={e => handleButtonSelect(e)} >{nat.replaceAll('_', ' ')}</button>
+                    ))}
+                
+                    <br></br>
+                    <br></br>
+                
+                    {/* <button type="button" onClick={() => evaluateChoice()} onKeyDown={() => evaluateChoice()}>Submit</button> */}
+                    <button type="button" onClick={() => nextChoice(difficulty)}>I give up! </button>
+                
+
                     
-                        <br></br>
-                    
-                        <button type="button" onClick={() => evaluateChoice()} onKeyDown={() => evaluateChoice()}>Submit</button>    <button type="button" onClick={() => updatePerson()}>Next choice </button>
-                    </form>
-      
+
+                    <br></br>
+                    <br></br>
+
+                    <button type="button" onClick={() => updateGameMode(HELTER_SKELTER, HELTER_SKELTER)}> Helter Skelter Mode </button> <button type="button" onClick={() => updateGameMode(SANDBOX, EASY)}> Easy </button> <button type="button" onClick={() => updateGameMode(SANDBOX, MEDIUM)}> Medium </button> <button type="button" onClick={() => updateGameMode(SANDBOX, HARD)}> Hard </button>
+                    <p className='score'> Score: {score} </p>
+                    {difficulty === HELTER_SKELTER ? null : <p className='difficulty'> Difficulty: {difficulty} </p> }
+                    <p className='gameMode'>Game mode: {gameMode === HELTER_SKELTER ? "Helter Skelter" : gameMode} </p>
+                    <p className='timer'>{gameMode === HELTER_SKELTER ? timeLeft : null} </p>
+
       </div>
     );
   }
--- src/game.js
+++ src/game.js
@@ -4,8 +4,10 @@
 import axios from 'axios';
 
 //make a server and just deploy it to render to continue testing
-var TEST_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Bundesarchiv_Bild_183-1985-0810-023,_Christine_Wachtel,_Hildegard_Körner-Ullrich.jpg/200px-Bundesarchiv_Bild_183-1985-0810-023,_Christine_Wachtel,_Hildegard_Körner-Ullrich.jpg";
 const backendURL = "https://raceplay.onrender.com";
+//const backendURL = "http://localhost:8080";
+const SECRET = process.env.SECRET;
+//const SECRET = "SECRET";
 
 export function getNextImage() {
 
@@ -20,18 +22,40 @@ export function getNextImage() {
 export async function getRandomPerson() {
     console.log(backendURL);
 
-    return await axios.get(backendURL + "/random")
+    return await axios.get(backendURL + "/random", {headers: {Authorization: SECRET}})
     .then(response => {
         console.log(response);
-        return response.data;})
+        return response.data;
+    })
 }
 
 
-export async function getNationalities() {
-    console.log(backendURL);
+export async function getNationalities(difficulty, person, score) {
+    console.log(JSON.stringify(person));
 
-    return await axios.get(backendURL + "/nationalities")
+    if ("helterSkelter" === difficulty) {
+
+        return getHelterSkelterNationalities(person, score);
+    }
+
+    return await axios.post(backendURL + "/nationalities" + "/" + difficulty, {data: JSON.stringify(person)},
+    {
+        headers: { 'Content-Type': 'application/json; charset=UTF-8', Authorization: SECRET },
+      })
     .then(response => {
         console.log(response);
-        return response.data;})
+        return response.data;
+    })
+}
+
+export async function getHelterSkelterNationalities(person, score) {
+
+    return await axios.post(backendURL + "/helterSkelter", {data: JSON.stringify(person), score: JSON.stringify(score)},
+    {
+        headers: { 'Content-Type': 'application/json; charset=UTF-8', Authorization: SECRET },
+      })
+    .then(response => {
+        console.log(response);
+        return response.data;
+    })
 }
