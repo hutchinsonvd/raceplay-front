272d91e044d0299c354e3fd813afa2c19bbe9e39
hutchinsonvd <vandykehutch@yahoo.com>
2025-01-01 15:55:38 -0500

    fixed score when changing game modes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ function App() {
   const MEDIUM = "medium";
   const HARD = "hard";
 
+  const CORRECT = "C";
+  const CORRECT_MESSAGE = ""
+  const WRONG = "W";
+  const WRONG_MESSAGE = "incorrect!";
+  const CORRECT_REGION = "CR";
+  const CORRECT_REGION_MESSAGE = "Same region but not the right country";
+
   const HELTER_SKELTER = "helterSkelter";
   const SANDBOX = "Sandbox";
   
@@ -33,6 +40,9 @@ function App() {
   const [timeLeft, setTimeLeft] = useState(60);
   const [gameMode, setGameMode] = useState(SANDBOX);
   const [showScoreModal, setShowScoreModal] = useState(false);
+  const [resultVisible, setResultVisible] = useState(false);
+  const [result, setResult] = useState(WRONG);
+
 
   useEffect(() => {
     if (score != 0) {
@@ -52,7 +62,8 @@ function App() {
     if (selection === person.nationality) {
 
       
-      alert('correct!');
+      //alert('correct!');
+      showResult(CORRECT_MESSAGE);
   
       setScore(score + 5);
     }
@@ -62,7 +73,8 @@ function App() {
       isSameRegion(person.nationality, selection)
       .then(result => {
         if (!result) {
-          alert('incorrect!');
+          //alert('incorrect!');
+          showResult(WRONG_MESSAGE)
 
           if (gameMode === HELTER_SKELTER) {
             setTimeLeft(t => t - 20);
@@ -74,6 +86,7 @@ function App() {
         }
         else {
           alert('Same region but not the right country')
+          showResult(CORRECT_REGION_MESSAGE)
 
           if (gameMode === HELTER_SKELTER) {
             setTimeLeft(t => t + 5);
@@ -111,6 +124,13 @@ function App() {
     }
   }
 
+  function showResult(result) {
+
+    setResult(result);
+    setResultVisible(true);
+    setTimeout(() => setResultVisible(false), 2000)
+  }
+
   async function showModalForHelterSkelter() {
     setRecentScore(score);
     setScore(0);
@@ -119,16 +139,22 @@ function App() {
 
   async function updateGameMode(gameMode, difficulty) {
 
-    return updatePersonAndNationalityList(difficulty)
+    setScore(0);
+
+    if (HELTER_SKELTER == gameMode) {
+
+      return updatePersonAndNationalityListForHelterSkelter()
       .then(data => setGameMode(gameMode))
       .then(data => {
         if (gameMode === HELTER_SKELTER) {
           
           setTimeLeft(60);
         }
+      })
+    }
 
-        setScore(0);
-      });
+    return updatePersonAndNationalityList(difficulty)
+      .then(data => setGameMode(gameMode));
   }
 
   async function nextChoice(difficulty) {
@@ -147,9 +173,21 @@ function App() {
 
     setPerson(data);
     setDifficulty(difficulty);
-
+    setSelection(null);
   
     return getNationalities(difficulty, data, score)
+        .then(data => setNationalities(data));
+    });
+  }
+
+  async function updatePersonAndNationalityListForHelterSkelter() {
+    return getRandomPerson().then(data => {
+
+    setPerson(data);
+    setDifficulty(HELTER_SKELTER);
+    setSelection(null);
+  
+    return getNationalities(HELTER_SKELTER, data, 0)
         .then(data => setNationalities(data));
     });
   }
@@ -196,14 +234,14 @@ function App() {
                     <img style={{ width: "22%", height: "22%" }} alt="hog" src={person.image_url} onError={() => logBrokenImageAndGetNewPerson()}id="image"/>
                     
                     <br></br>
-                    <label htmlFor="nationalities">Choose a nationality: </label>
+                    <label style={{fontSize: '20px' }} htmlFor="nationalities">Choose a nationality: </label>
 
                     <br></br>
                     <br></br>
               
 
                     {nationalities.map(nat => (
-                      <button style={{ marginLeft: '.25rem' }} key={nat} value={nat} onClick={e => handleButtonSelect(e)} >{nat.replaceAll('_', ' ')}</button>
+                      <button style={{ marginLeft: '.25rem', fontSize: '20px' }} key={nat} onClick={e => handleButtonSelect(e)} >{nat.replaceAll('_', ' ')}</button>
                     ))}
                 
                     <br></br>
@@ -228,7 +266,7 @@ function App() {
 
                     <button type="button" onClick={() => updateGameMode(HELTER_SKELTER, HELTER_SKELTER)}> Helter Skelter Mode </button> <button type="button" onClick={() => updateGameMode(SANDBOX, EASY)}> Easy </button> <button type="button" onClick={() => updateGameMode(SANDBOX, MEDIUM)}> Medium </button> <button type="button" onClick={() => updateGameMode(SANDBOX, HARD)}> Hard </button>
                     
-                    <p className='score'> Score: {score} </p>
+                    <p className='score' style={{fontSize: '20px' }}> Score: {score} </p>
                     {difficulty === HELTER_SKELTER ? null : <p className='difficulty'> Difficulty: {difficulty} </p> }
                     <p className='gameMode'>Game mode: {gameMode === HELTER_SKELTER ? "Helter Skelter" : gameMode} </p>
                     <p className='timer'>{gameMode === HELTER_SKELTER ? timeLeft : null} </p>
