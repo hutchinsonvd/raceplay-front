c4639699861abc2d7f8bd7681ac0a247c724bb44
hutchinsonvd <vandykehutch@yahoo.com>
2025-01-02 14:59:16 -0500

    encrypted payloads

---
+++ src/crypt.js
@@ -0,0 +1,44 @@
+import crypto from "crypto-js"
+
+//const secret_key = process.env.SECRET_KEY //prod only
+const secret_key = "abcd"
+
+
+export function encryptData(data) {
+    console.log(data);
+
+    var a = {
+        id: encrypt(data.id),
+        label: encrypt(data.label),
+        nationality: encrypt(data.nationality),
+        year_of_birth: encrypt(data.year_of_birth),
+        image_url: encrypt(data.image_url)
+    }
+
+    var b = decryptPerson(a);
+
+    console.log(a);
+
+    console.log(b);
+
+    return a;
+  }
+
+  function encrypt(data) {
+    return crypto.AES.encrypt(data, secret_key).toString();
+  }
+
+  function decrypt(encryptedData) {
+    return crypto.AES.decrypt(encryptedData, secret_key).toString(crypto.enc.Utf8)
+}
+
+export function decryptPerson(data) {
+
+    return {
+        id: decrypt(data.id),
+        label: decrypt(data.label),
+        nationality: decrypt(data.nationality),
+        year_of_birth: decrypt(data.year_of_birth),
+        image_url: decrypt(data.image_url)
+    }
+}
